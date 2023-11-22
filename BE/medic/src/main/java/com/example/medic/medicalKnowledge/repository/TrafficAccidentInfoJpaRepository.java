76b4f80d043ad87ca0f3250008c3d43ba679d376
dudxo <father8475@naver.com>
2023-11-23 01:10:22 +0900

    [Fix] TrafficAccidentInfoRepository 오류 수정

--- BE/medic/src/main/java/com/example/medic/medicalKnowledge/repository/TrafficAccidentInfoJpaRepository.java
+++ BE/medic/src/main/java/com/example/medic/medicalKnowledge/repository/TrafficAccidentInfoJpaRepository.java
@@ -1,6 +1,7 @@
 package com.example.medic.medicalKnowledge.repository;
 
+import com.example.medic.medicalKnowledge.domain.TrafficAccidentInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TrafficAccidentInfoJpaRepository extends JpaRepository<TrafficAccidentInfoJpaRepository,Long> {
+public interface TrafficAccidentInfoJpaRepository extends JpaRepository<TrafficAccidentInfo, Long> {
 }
