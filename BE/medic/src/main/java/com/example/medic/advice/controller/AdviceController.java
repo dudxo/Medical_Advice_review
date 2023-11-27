8ecfd1ce6f32ceea04317bc24a9852168921fd35
dudxo <father8475@naver.com>
2023-11-27 17:31:03 +0900

    [Feat] 자문의뢰 신청 저장 REST API 구현

--- BE/medic/src/main/java/com/example/medic/advice/controller/AdviceController.java
+++ BE/medic/src/main/java/com/example/medic/advice/controller/AdviceController.java
@@ -1,11 +1,17 @@
 package com.example.medic.advice.controller;
 
 import com.example.medic.advice.dto.AllAdviceRequestDto;
+import com.example.medic.advice.service.AdviceService;
+import com.example.medic.client.dto.ClientInfoDto;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @RestController
 @AllArgsConstructor
@@ -14,7 +20,17 @@ public class AdviceController {
     private final AdviceService adviceService;
 
     @PostMapping("/advice/request")
-    public ResponseEntity<String> saveAdviceRequest(@RequestBody AllAdviceRequestDto allAdviceRequestDto) {
-        String result = adviceService.saveAdviceReqeust(allAdviceRequestDto);
+    public ResponseEntity<String> saveAdviceRequest(@RequestBody AllAdviceRequestDto allAdviceRequestDto,
+                                                    HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        String uid = (String) session.getAttribute("uId");
+
+        ClientInfoDto clientInfoDto = ClientInfoDto.builder()
+                .uId(uid)
+                .build();
+        if (adviceService.saveAdviceRequest(allAdviceRequestDto, clientInfoDto)) {
+          return  ResponseEntity.ok().body("saved");
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed");
     }
 }
