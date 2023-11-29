fefbb1e6761b0455f1cf0fa674a2d603a9e6b0ad
rudgns328 <128586833+rudgns328@users.noreply.github.com>
2023-11-30 02:28:58 +0900

    [Feat] 마이페이지 분석의뢰 건수 조회(레포지토리)

--- BE/medic/src/main/java/com/example/medic/analyze/repository/AnalyzeRequestListRepository.java
+++ BE/medic/src/main/java/com/example/medic/analyze/repository/AnalyzeRequestListRepository.java
@@ -1,7 +1,14 @@
 package com.example.medic.analyze.repository;
 
+import com.example.medic.advice.domain.AdviceRequestList;
 import com.example.medic.analyze.domain.AnalyzeRequestList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AnalyzeRequestListRepository extends JpaRepository<AnalyzeRequestList,Long> {
+
+    @Query("SELECT arl FROM AnalyzeRequestList arl WHERE arl.client.uId = :uId")
+    List<AnalyzeRequestList> findByClient_UId(String uId);
 }
