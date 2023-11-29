ed6ef68644691fc56462dda7c02783c9e765f65a
rudgns328 <128586833+rudgns328@users.noreply.github.com>
2023-11-30 02:31:32 +0900

    [Feat] 마이페이지 번역의뢰 건수 조회(레포지토리)

--- BE/medic/src/main/java/com/example/medic/translation/repository/TranslationRequestListRepository.java
+++ BE/medic/src/main/java/com/example/medic/translation/repository/TranslationRequestListRepository.java
@@ -1,7 +1,14 @@
 package com.example.medic.translation.repository;
 
+import com.example.medic.analyze.domain.AnalyzeRequestList;
 import com.example.medic.translation.domain.TranslationRequestList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TranslationRequestListRepository extends JpaRepository<TranslationRequestList,Long> {
+
+    @Query("SELECT trl FROM TranslationRequestList trl WHERE trl.client.uId = :uId")
+    List<TranslationRequestList> findByClient_UId(String uId);
 }
