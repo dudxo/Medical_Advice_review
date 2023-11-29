a4a0d680b520cff33d4dd0bc75be28429e33714c
rudgns328 <128586833+rudgns328@users.noreply.github.com>
2023-11-30 02:30:30 +0900

    [Feat] 마이페이지 번역의뢰 건수 조회(컨트롤러)

--- BE/medic/src/main/java/com/example/medic/translation/controller/TranslationSituationController.java
+++ BE/medic/src/main/java/com/example/medic/translation/controller/TranslationSituationController.java
@@ -1,2 +1,37 @@
-package com.example.medic.translation.controller;public class TranslationSituationController {
+package com.example.medic.translation.controller;
+
+import com.example.medic.translation.service.TranslationSituationService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@RestController
+@AllArgsConstructor
+public class TranslationSituationController {
+
+    private final TranslationSituationService translationSituationService;
+
+    @GetMapping("mypage/myTranslationSituation")
+    public ResponseEntity<Integer> getTranslationCount(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        String currentUid = (String) session.getAttribute("uId");
+
+        if (currentUid == null) {
+            // 세션에 사용자 ID가 없으면 세션이 유효하지 않음
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+
+        try {
+            // AdviceRequestService를 통해 자문의뢰 건수 조회
+            int translationRequestCount = translationSituationService.getTranslationCount(currentUid);
+            return ResponseEntity.ok(translationRequestCount);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
 }
