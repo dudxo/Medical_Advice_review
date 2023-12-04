a5317089f52b83be8c1cffa6850bf294ea80fcc1
rudgns328 <128586833+rudgns328@users.noreply.github.com>
2023-12-04 22:27:58 +0900

    번역 의뢰 현황 구현 (DTO)

--- BE/medic/src/main/java/com/example/medic/translation/dto/TranslationSituationDto.java
+++ BE/medic/src/main/java/com/example/medic/translation/dto/TranslationSituationDto.java
@@ -1,2 +1,25 @@
-package com.example.medic.translation.dto;public class TranslationSituationDto {
+package com.example.medic.translation.dto;
+
+import com.example.medic.translation.domain.TranslationRequestList;
+import com.example.medic.translation.dto.TranslationSituationDto;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+@Builder
+public class TranslationSituationDto {
+
+    private String trPtSub;
+    private String trPtDiagnosis;
+    private Date trRegDate;
+
+    public static TranslationSituationDto from(TranslationRequestList translationRequestList) {
+        return TranslationSituationDto.builder()
+                .trPtSub(translationRequestList.getTrPtSub())
+                .trPtDiagnosis(translationRequestList.getTrPtDiagnosis())
+                .trRegDate(translationRequestList.getTrRegDate())
+                .build();
+    }
 }
