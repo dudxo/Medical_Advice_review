8026ea4df51d2fb05638c02feb32d8df901d288a
박민서 <qkrals2475@naver.com>
2023-12-28 21:36:55 +0900

    [feat] 관리자 페이지 js

---
+++ FE/medic/src/pages/Adminstrator/AdministratorMypage.js
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from "react";
+import administrator from '../../css/AdministratorMypage.module.css'
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+export default function AdministratorMypage() {
+    const [userCount, setUserCount] = useState(0);
+    const [consultativeCount, setConsultativeCount] = useState(0);
+    const [qnaCount, setQnaCount] = useState(0);
+    const [myAdvice, setMyAdvice] = useState(0)
+    const [myanalysis, setMyAnalysis] = useState(0)
+    const [myTranslation, setMyTranslation] = useState(0)
+
+    const navigate = useNavigate();
+
+    const myRequestcount = async () => {
+        try {
+            const userResponse = await axios.get('/administrator/countUser');
+            setUserCount(userResponse.data);
+
+            const consultativeResponse = await axios.get('/administrator/countConsultative');
+            setConsultativeCount(consultativeResponse.data);
+
+            const advice = await axios.get('/mypage/myAdviceSituation')
+            setMyAdvice(advice.data)
+            const Analysis = await axios.get('/mypage/myAnalyzeSituation')
+            setMyAnalysis(Analysis.data)
+            const Translation = await axios.get('/mypage/myTranslationSituation')
+            setMyTranslation(Translation.data)
+            
+            const qnaResponse = await axios.get('/administrator/qnaCount');
+            setQnaCount(qnaResponse.data);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    const btn_show_doc = e => {
+        navigate('/medic/adminstrator/docmanagement')
+    }
+    
+    const btn_show_user = e => {
+        navigate('/medic/adminstrator/usermanagement')
+    }
+
+
+    useEffect(() => {
+        // 페이지가 마운트될 때 데이터를 가져오도록 useEffect를 사용
+        myRequestcount();
+    }, [])
+
+    return (
+        <div className={administrator.adminMypage_box}>
+            <div className={administrator.adminMypage_requestcount_box}>
+                <div className={administrator.adminMypage_count_wrap}>
+                    <div className={administrator.adminMypage_countbox} onClick={btn_show_user}>
+                        <h2 className={administrator.adminMypage_my_counttitle}>회원 관리</h2>
+                        <div className={administrator.adminMy_count}>
+                            <h3>전체 {userCount}명</h3>
+                        </div>
+                    </div>
+                    <div className={administrator.adminMypage_countbox} onClick={btn_show_doc}>
+                        <h2 className={administrator.adminMypage_my_counttitle}>전문의 관리</h2>
+                        <div className={administrator.adminMy_count}>
+                            <h3>전체 {consultativeCount}명</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={administrator.adminMypage_count_wrap}>
+                    <div className={administrator.adminMypage_countbox}>
+                        <h2 className={administrator.adminMypage_my_counttitle}>자문 현황 관리</h2>
+                        <div className={administrator.adminMy_count}>
+                            <h3>전체 {qnaCount} 건</h3>
+                        </div>
+                    </div>
+                    <div className={administrator.adminMypage_countbox}>
+                        <h2 className={administrator.adminMypage_my_counttitle}>Qna 관리</h2>
+                        <div className= {administrator.adminMy_count}>
+                            <h3>전체 {myAdvice} 건</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={administrator.adminMypage_count_wrap}>
+                    <div className={administrator.adminMypage_countbox} >
+                        <h2 className={administrator.adminMypage_my_counttitle}>분석 현황 관리</h2>
+                        <div className={administrator.adminMy_count}>
+                            <h3>전체 {myanalysis}건</h3>
+                        </div>
+                    </div>
+                    <div className={administrator.adminMypage_countbox} >
+                        <h2 className={administrator.adminMypage_my_counttitle}>번역 현황 관리</h2>
+                        <div className={administrator.adminMy_count}>
+                            <h3>전체 {myTranslation}명</h3>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
---
+++ FE/medic/src/pages/Adminstrator/UserManagement.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import usermanage from '../../css/UserManagement.module.css';
+
+export default function DocManagement() {
+  const navigate = useNavigate();
+  const [selectedStatu, setSelectedStatu] = useState('회원관리');
+  const [userList, setUserList] = useState([]);
+  const [Page, setPage] = useState(1);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/user/management');
+        setUserList(response.data);
+      } catch (error) {
+        console.error('회원 정보 가져오기 오류', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleStatusChange = (newStatus) => {
+    setSelectedStatu(newStatus);
+  };
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  }
+
+  const handleEditUser = (selectedUser) => {
+    localStorage.setItem('selectedUser', JSON.stringify(selectedUser));
+    navigate('/edit/user', { state: { user: selectedUser } });
+  }
+
+  const handleDeleteUser = async (userId) => {
+    try {
+      const response = await axios.delete(`/user/delete/${userId}`);
+      
+      if (response.status === 200) {
+        const updatedUserList = userList.filter(user => user.id !== userId);
+        setUserList(updatedUserList);
+        alert('사용자가 삭제되었습니다.');
+      } else {
+        alert('사용자 삭제에 실패했습니다.');
+      }
+    } catch (error) {
+      console.error('사용자 삭제 오류', error);
+      alert('사용자 삭제 중 오류가 발생했습니다.');
+    }
+  }
+
+  return (
+    <div className={usermanage.user_contents}>
+      <h1>회원 관리</h1>
+      <table className={usermanage.user_table}>
+        <thead>
+          <tr>
+            <th className={usermanage.user_th}>NO.</th>
+            <th className={usermanage.user_th}>이름</th>
+            <th className={usermanage.user_th}>구분</th>
+            <th className={usermanage.user_th}>수정</th>
+            <th className={usermanage.user_th}>삭제</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userList.map((user, index) => (
+            <tr key={index}>
+              <td className={usermanage.user_td}>{index + 1}</td>
+              <td className={usermanage.user_td}>{user.uName}</td>
+              <td className={usermanage.user_td}>{user.uRole}</td>
+              <td className={usermanage.doc_td} onClick={() => handleEditUser(user)}>수정</td>
+              <td className={usermanage.doc_td} onClick={() => handleDeleteUser(user.uId)}>삭제</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className={usermanage.user_pagination}>
+        <button className={usermanage.user_paginationButton} onClick={() => handlePageChange(Page - 1)} disabled={Page === 1}>
+          ◀
+        </button>
+        {[...Array(10)].map((_, index) => (
+          <button key={index} className={usermanage.user_paginationButton} onClick={() => handlePageChange(index + 1)} disabled={Page === index + 1}>
+            {index + 1}
+          </button>
+        ))}
+        <button className={usermanage.user_paginationButton} onClick={() => handlePageChange(Page + 1)}>
+          ▶
+        </button>
+      </div>
+    </div>
+  );
+};
