a756d16df5d71d8dde5aa73882696cc057e20341
dudxo <father8475@naver.com>
2024-01-09 14:10:54 +0900

    [Feat] 사용자의 분석의뢰 건수 조회 JPA 추가

--- BE/medic/src/main/java/com/example/medic/analyze/repository/AnalyzeRequestListRepository.java
+++ BE/medic/src/main/java/com/example/medic/analyze/repository/AnalyzeRequestListRepository.java
@@ -2,6 +2,7 @@ package com.example.medic.analyze.repository;
 
 import com.example.medic.advice.domain.AdviceRequestList;
 import com.example.medic.analyze.domain.AnalyzeRequestList;
+import com.example.medic.client.domain.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,4 +12,6 @@ public interface AnalyzeRequestListRepository extends JpaRepository<AnalyzeReque
 
     @Query("SELECT arl FROM AnalyzeRequestList arl WHERE arl.client.uId = :uId")
     List<AnalyzeRequestList> findByClient_UId(String uId);
+
+    int countAllByClient(Client client);
 }
