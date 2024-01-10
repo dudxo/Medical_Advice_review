099be78592360a7f2b01b9a24ea841b6487c804c
dudxo <father8475@naver.com>
2024-01-10 17:20:39 +0900

    [Test] 관리자 전문의 목록확인 테스트 코드 작성

--- BE/medic/src/test/java/com/example/medic/manage/consultative/ConsultativeManagementServiceImplTest.java
+++ BE/medic/src/test/java/com/example/medic/manage/consultative/ConsultativeManagementServiceImplTest.java
@@ -1,2 +1,83 @@
-package com.example.medic.manage.consultative;public class ConsultattiveManagementServiceImplTest {
+package com.example.medic.manage.consultative;
+
+import com.example.medic.consultative.domain.Consultative;
+import com.example.medic.consultative.repository.ConsultativeRepository;
+import com.example.medic.manager.dto.ManagedClientInfoDto;
+import com.example.medic.manager.dto.ManagedConsultativeInfoDto;
+import com.example.medic.manager.service.ConsultativeManagementServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class ConsultativeManagementServiceImplTest {
+
+    @Autowired
+    ConsultativeManagementServiceImpl consultativeManagementService;
+    @Autowired
+    ConsultativeRepository consultativeRepository;
+
+    @BeforeEach
+    @DisplayName("전문의 등록")
+    @Transactional
+    void insertConsultative() {
+        Consultative insertConsultative1 = consultativeRepository.save(Consultative.builder()
+                .cId("test1")
+                .cPw("1")
+                .cRole("전문의")
+                .cName("abcd")
+                .cAddress("ad")
+                .cEmail("test@test.com")
+                .cPhone("111")
+                .cTel("111")
+                .hospAddress("111")
+                .department("11")
+                .hospFx("11")
+                .hospName("11")
+                .hospNum("11")
+                .hospTel("11")
+                .build());
+
+        Consultative insertConsultative2 = consultativeRepository.save(Consultative.builder()
+                .cId("test2")
+                .cPw("2")
+                .cRole("전문의2")
+                .cName("abcd2")
+                .cAddress("ad2")
+                .cEmail("test2@test.com")
+                .cPhone("1112")
+                .cTel("1112")
+                .hospAddress("1112")
+                .department("112")
+                .hospFx("112")
+                .hospName("112")
+                .hospNum("112")
+                .hospTel("112")
+                .build());
+    }
+
+    @Test
+    @DisplayName("전문의 목록 확인")
+    @Transactional
+    void findAllConsultative() {
+        List<ManagedConsultativeInfoDto> allConsultative = consultativeManagementService.findAllConsultative();
+        for (ManagedConsultativeInfoDto managedConsultativeInfoDto : allConsultative) {
+            Consultative findConsultative = consultativeRepository.findById(managedConsultativeInfoDto.getCId()).get();
+            if (findConsultative.getCId().equals("test1")) {
+                assertThat(findConsultative.getCEmail()).isEqualTo("test@test.com");
+                assertThat(managedConsultativeInfoDto.getCountByAdviceAssignment()).isEqualTo(0);
+                assertThat(managedConsultativeInfoDto.getCountByAdviceAssignment()).isNotEqualTo(3);
+            } else {
+                assertThat(findConsultative.getCEmail()).isEqualTo("test2@test.com");
+            }
+        }
+    }
 }
