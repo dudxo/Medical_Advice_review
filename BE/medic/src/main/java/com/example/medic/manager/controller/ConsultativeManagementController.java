c5303dc79b5c08881810346d9db0a443620f4fed
dudxo <father8475@naver.com>
2024-01-11 13:42:58 +0900

    [Feat] 전문의 목록 조회 컨트롤러 추가(API 구현X)

--- BE/medic/src/main/java/com/example/medic/manager/controller/ConsultativeManagementController.java
+++ BE/medic/src/main/java/com/example/medic/manager/controller/ConsultativeManagementController.java
@@ -1,8 +1,13 @@
 package com.example.medic.manager.controller;
 
+import com.example.medic.manager.dto.ManagedConsultativeInfoDto;
 import com.example.medic.manager.service.ConsultativeManagementServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -10,5 +15,15 @@ public class ConsultativeManagementController {
 
     private final ConsultativeManagementServiceImpl consultativeManagementService;
 
+    /**
+     * @return 전문의 목록 조회
+     */
+    public ResponseEntity<List<ManagedConsultativeInfoDto>> findAllConsultative() {
+        List<ManagedConsultativeInfoDto> response = consultativeManagementService.findAllConsultative();
+        if (response == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        return ResponseEntity.ok(response);
+    }
 
 }
