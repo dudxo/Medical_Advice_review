7fc434c0b37b31b83d11e691d0ca5822b1120e43
dudxo <father8475@naver.com>
2024-01-12 14:29:23 +0900

    [Feat] 분석의뢰 저장 테스트를 위한 각 조회 JPA 코드 추가

--- BE/medic/src/main/java/com/example/medic/analyze/repository/AnalyzeRequestFileRepository.java
+++ BE/medic/src/main/java/com/example/medic/analyze/repository/AnalyzeRequestFileRepository.java
@@ -1,7 +1,12 @@
 package com.example.medic.analyze.repository;
 
+import com.example.medic.analyze.domain.AnalyzeRequest;
 import com.example.medic.analyze.domain.AnalyzeRequestFile;
+import com.example.medic.analyze.domain.AnalyzeRequestList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnalyzeRequestFileRepository extends JpaRepository<AnalyzeRequestFile, Long> {
+
+    AnalyzeRequestFile findByAnalyzeRequestList(AnalyzeRequestList analyzeRequestList);
+
 }
--- BE/medic/src/main/java/com/example/medic/analyze/repository/AnalyzeRequestRepository.java
+++ BE/medic/src/main/java/com/example/medic/analyze/repository/AnalyzeRequestRepository.java
@@ -1,7 +1,12 @@
 package com.example.medic.analyze.repository;
 
 import com.example.medic.analyze.domain.AnalyzeRequest;
+import com.example.medic.analyze.domain.AnalyzeRequestList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AnalyzeRequestRepository extends JpaRepository<AnalyzeRequest,Long> {
+
+    List<AnalyzeRequest> findAllByAnalyzeRequestList(AnalyzeRequestList analyzeRequestList);
 }
