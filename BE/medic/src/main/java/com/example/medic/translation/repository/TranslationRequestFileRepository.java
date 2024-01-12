619c611077976555251b81bd16cea640442f5580
dudxo <father8475@naver.com>
2024-01-12 17:23:40 +0900

    [Feat] 번역 의뢰 저장 테스트 관련 JPA 코드 추가

--- BE/medic/src/main/java/com/example/medic/translation/repository/TranslationRequestFileRepository.java
+++ BE/medic/src/main/java/com/example/medic/translation/repository/TranslationRequestFileRepository.java
@@ -1,7 +1,10 @@
 package com.example.medic.translation.repository;
 
 import com.example.medic.translation.domain.TranslationRequestFile;
+import com.example.medic.translation.domain.TranslationRequestList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TranslationRequestFileRepository extends JpaRepository<TranslationRequestFile,Long> {
+
+    TranslationRequestFile findByTranslationRequestList(TranslationRequestList translationRequestList);
 }
