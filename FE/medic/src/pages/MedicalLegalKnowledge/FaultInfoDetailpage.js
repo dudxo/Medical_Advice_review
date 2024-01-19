f742b2b9c520d9747594aa480b2a7e8d81adda8d
rudgns328 <128586833+rudgns328@users.noreply.github.com>
2024-01-20 02:19:32 +0900

    [Feat] 의료과실 정보 저장, 목록, 상세 구현 JS

--- FE/medic/src/pages/MedicalLegalKnowledge/FaultInfoDetailpage.js
+++ FE/medic/src/pages/MedicalLegalKnowledge/FaultInfoDetailpage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import faultInfoDetail from '../../css/FaultInfoDetailpage.module.css';
+import faultinfodetails from '../../css/FaultInfoDetailpage.module.css';
 import axios from 'axios';
 import { useLocation, useNavigate } from 'react-router-dom';
 
@@ -7,12 +7,14 @@ export default function FaultInfoDetailpage(){
   const navigate = useNavigate();
   const location = useLocation();
 
-  const faultInfoDetail = location.state.faultInfoDetail; // 상세 게시글 번호
+  const medicalNegligenceInfo = location.state.faultInfoDetail; // 상세 게시글 번호
   const faultInfoId = location.state.faultInfoId;   // 상세 게시글 번호 리스트
   const faultInfos = location.state.faultInfos;   // 의료과실정보 리스트
 
   const [prevTitle, setPrevTitle] = useState('');
   const [nextTitle, setNextTitle] = useState('');
+  const [prevWriter, setPrevWriter] = useState('');
+  const [nextWriter, setNextWriter] = useState('');
   const [prevDate, setPrevDate] = useState('');
   const [nextDate, setNextDate] = useState('');
 
@@ -20,13 +22,15 @@ export default function FaultInfoDetailpage(){
     if (faultInfoId === 0) {
       setNextTitle('이전글이 없습니다.');
     } else {
-      setNextTitle(faultInfos[faultInfoId - 1].fiName);
-      setNextDate(faultInfos[faultInfoId - 1].fiRegdate);
+      setNextTitle(faultInfos[faultInfoId - 1].mnName);
+      setNextWriter(faultInfos[faultInfoId - 1].mnInstitution)
+      setNextDate(faultInfos[faultInfoId - 1].mnRegdate);
     }
 
     if (faultInfoId < faultInfos.length - 1) {
-      setPrevTitle(faultInfos[faultInfoId + 1].fiName);
-      setPrevDate(faultInfos[faultInfoId + 1].fiRegdate);
+      setPrevTitle(faultInfos[faultInfoId + 1].mnName);
+      setPrevWriter(faultInfos[faultInfoId + 1].mnInstitution)
+      setPrevDate(faultInfos[faultInfoId + 1].mnRegdate);
     } else {
       setPrevTitle('다음글이 없습니다.');
     }
@@ -43,8 +47,8 @@ export default function FaultInfoDetailpage(){
   };
 
   return (
-    <div className={faultInfoDetail.detailform}>
-      <div className={faultInfoDetail.detail_title}>
+    <div className={faultinfodetails.detailform}>
+      <div className={faultinfodetails.detail_title}>
         <h2>
           <i className="fa-solid fa-circle icon"></i>
           의료과실정보 상세 보기
@@ -52,39 +56,45 @@ export default function FaultInfoDetailpage(){
       </div>
       <br />
       <form>
-        <table className={faultInfoDetail.faultinfodetail_table}>
+        <table className={faultinfodetails.faultinfodetail_table}>
           <tr>
-            <th className={faultInfoDetail.faultinfodetail_th}>제목</th>
-            <td className={faultInfoDetail.faultinfodetail_td}>{faultInfoDetail.fiName}</td>
-            <th className={faultInfoDetail.faultinfodetail_th}>등록일</th>
-            <td className={faultInfoDetail.faultinfodetail_td}>{formatDateString(faultInfoDetail.fiRegDate)}</td>
+            <th className={faultinfodetails.faultinfodetail_th}>제목</th>
+            <td className={faultinfodetails.faultinfodetail_td}>{medicalNegligenceInfo.mnName}</td>
+            <th className={faultinfodetails.faultinfodetail_th}>등록일</th>
+            <td className={faultinfodetails.faultinfodetail_td}>{formatDateString(medicalNegligenceInfo.mnRegdate)}</td>
           </tr>
-          <th className={faultInfoDetail.faultinfodetail_th}>내용</th>
-          <td colSpan="3" className={faultInfoDetail.faultinfodetail_td}>
-            <div className={faultInfoDetail.content}>{faultInfoDetail.fiContent}</div>
+          <th className={faultinfodetails.faultinfodetail_th}>내용</th>
+          <td colSpan="3" className={faultinfodetails.faultinfodetail_td}>
+            <div className={faultinfodetails.content}>{medicalNegligenceInfo.mnContent}</div>
           </td>
           <tr></tr>
         </table>
         <br />
-        <div className={faultInfoDetail.secondTable}>
-          <table className={faultInfoDetail.faultinfodetail_table}>
+        <div className={faultinfodetails.secondTable}>
+          <table className={faultinfodetails.faultinfodetail_table}>
             <tr>
-              <th className={faultInfoDetail.faultinfodetail_th}>이전글</th>
-              <td className={faultInfoDetail.faultinfodetail_td}>{prevTitle}</td>
-              <td className={faultInfoDetail.faultinfodetail_td}>건강관리공단</td>
-              <td className={faultInfoDetail.faultinfodetail_td}>{formatDateString(prevDate)}</td>
+              <th className={faultinfodetails.faultinfodetail_th}>이전글</th>
+              <td className={faultinfodetails.faultinfodetail_td}>{prevTitle}</td>
+              <td className={faultinfodetails.faultinfodetail_td}>{prevWriter}</td>
+              <td className={faultinfodetails.faultinfodetail_td}>{formatDateString(prevDate)}</td>
             </tr>
             <tr>
-              <th className={faultInfoDetail.faultinfodetail_th}>다음글</th>
-              <td className={faultInfoDetail.faultinfodetail_td}>{nextTitle}</td>
-              <td className={faultInfoDetail.faultinfodetail_td}>건강관리공단</td>
-              <td className={faultInfoDetail.faultinfodetail_td}>{formatDateString(nextDate)}</td>
+              <th className={faultinfodetails.faultinfodetail_th}>다음글</th>
+              <td className={faultinfodetails.faultinfodetail_td}>{nextTitle}</td>
+              <td className={faultinfodetails.faultinfodetail_td}>{nextWriter}</td>
+              <td className={faultinfodetails.faultinfodetail_td}>{formatDateString(nextDate)}</td>
             </tr>
           </table>
         </div>
-        <div className={faultInfoDetail.complete}>
-          <button type="button" onClick={medicfaultInfo} className={faultInfoDetail.btt_write}>
+        <div className={faultinfodetails.complete}>
+          <button type="button" onClick={medicfaultInfo} className={faultinfodetails.btt_write}>
             목록
+          </button>
+          <button type="button" onClick={medicfaultInfo} className={faultinfodetails.btt_write}>
+            수정
+          </button>
+          <button type="button" onClick={medicfaultInfo} className={faultinfodetails.btt_write}>
+            삭제
           </button>
         </div>
       </form>
