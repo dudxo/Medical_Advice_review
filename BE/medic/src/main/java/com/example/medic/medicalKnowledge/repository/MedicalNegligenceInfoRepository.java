7e09a962d6902a274ce4a4467436d4d545f6f70c
rudgns328 <128586833+rudgns328@users.noreply.github.com>
2024-01-22 02:09:27 +0900

    [Feat] 의료과실정보 검색 기능 구현 Repository

--- BE/medic/src/main/java/com/example/medic/medicalKnowledge/repository/MedicalNegligenceInfoRepository.java
+++ BE/medic/src/main/java/com/example/medic/medicalKnowledge/repository/MedicalNegligenceInfoRepository.java
@@ -3,5 +3,12 @@ package com.example.medic.medicalKnowledge.repository;
 import com.example.medic.medicalKnowledge.domain.MedicalNegligenceInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MedicalNegligenceInfoRepository extends JpaRepository<MedicalNegligenceInfo,Long> {
+
+    /**
+     * 검색 기능
+     */
+    List<MedicalNegligenceInfo> findByMnNameContaining(String keyword);
 }
