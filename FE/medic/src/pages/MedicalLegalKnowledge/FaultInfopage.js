ff243353e3c490d77b33ae3f408d7eb206ab4dcb
rudgns328 <128586833+rudgns328@users.noreply.github.com>
2024-01-22 02:10:56 +0900

    [Feat] 의료과실정보 검색 기능 구현 JS

--- FE/medic/src/pages/MedicalLegalKnowledge/FaultInfopage.js
+++ FE/medic/src/pages/MedicalLegalKnowledge/FaultInfopage.js
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 
 export default function FaultInfopage(){
     const [faultInfos, setFaultInfos] = useState([]);
+    const [searchKeyword, setSearchKeyword] = useState("");
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -21,6 +22,16 @@ export default function FaultInfopage(){
 
         getFaultInfos();
   }, []);
+
+  const searchMedicalNegligenceInfo = async () => {
+    try {
+      const resp = await axios.get(`/search/mninfo?keyword=${searchKeyword}`);
+      const data = resp.data;
+      setFaultInfos(data);
+    } catch (error) {
+      console.error('의료과실 정보 검색:', error);
+    }
+  };
 
   const formatDateString = (dateString) => {
     const date = new Date(dateString);
@@ -50,6 +61,15 @@ export default function FaultInfopage(){
             의료과실정보
           </h2>
         </div>
+        <div>
+        <input
+          type="text"
+          placeholder="검색어를 입력하세요"
+          value={searchKeyword}
+          onChange={(e) => setSearchKeyword(e.target.value)}
+        />
+        <button onClick={searchMedicalNegligenceInfo}>검색</button>
+      </div>
         <br />
         <div className={faultInfo.tb}>
           <table className={faultInfo.faultInfo_table}>
