81a35a1cf9d3b43bcd9db662da8cf267e3d8a070
rudgns328 <128586833+rudgns328@users.noreply.github.com>
2024-01-26 20:14:24 +0900

    [Feat] 분석의뢰 상세, 현황 조회 구현 Dto

--- BE/medic/src/main/java/com/example/medic/analyze/dto/AnalyzeSituationDto.java
+++ BE/medic/src/main/java/com/example/medic/analyze/dto/AnalyzeSituationDto.java
@@ -1,7 +1,7 @@
 package com.example.medic.analyze.dto;
 
-import com.example.medic.advice.domain.AdviceRequestList;
-import com.example.medic.advice.dto.AdviceSituationDto;
+import com.example.medic.analyze.domain.AnalyzeRequestList;
+import com.example.medic.analyze.dto.AnalyzeSituationDto;
 import com.example.medic.analyze.domain.AnalyzeRequestList;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,17 +13,22 @@ import java.util.Date;
 @Builder
 public class AnalyzeSituationDto {
 
+    private Long anId;
     private String anPtSub;
     private String anPtDiagnosis;
     private LocalDate anRegDate;
+    private LocalDate adMdDate;
+    private LocalDate anAnswerDate;
+    private String anProgressStatus;
 
-    private Long anId;
-
-    public static AnalyzeSituationDto from(AnalyzeRequestList analyzeRequestList) {
-        return AnalyzeSituationDto.builder()
-                .anPtSub(analyzeRequestList.getAnPtSub())
-                .anPtDiagnosis(analyzeRequestList.getAnPtDiagnosis())
-                .anRegDate(analyzeRequestList.getAnRegDate())
-                .build();
+    public AnalyzeSituationDto(Long anId, String anPtSub, String anPtDiagnosis, LocalDate anRegDate, LocalDate adMdDate,
+                               LocalDate anAnswerDate, String anProgressStatus) {
+        this.anId = anId;
+        this.anPtSub = anPtSub;
+        this.anPtDiagnosis = anPtDiagnosis;
+        this.anRegDate = anRegDate;
+        this.adMdDate = adMdDate;
+        this.anAnswerDate = anAnswerDate;
+        this.anProgressStatus = anProgressStatus;
     }
 }
