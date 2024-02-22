d5c3f8b8f6ca87814b63e07fa5468fbd72a2d398
dudxo <father8475@naver.com>
2024-02-22 17:15:57 +0900

    [Feat] 00의뢰 답변 배정 쿼리문 추가

--- BE/medic/src/main/java/com/example/medic/translation/repository/TranslationAnswerFileRepository.java
+++ BE/medic/src/main/java/com/example/medic/translation/repository/TranslationAnswerFileRepository.java
@@ -1,12 +1,15 @@
 package com.example.medic.translation.repository;
 
 import com.example.medic.analyze.domain.AnalyzeAssignment;
+import com.example.medic.analyze.domain.AnalyzeRequestList;
 import com.example.medic.translation.domain.TranslationAnswerFile;
 import com.example.medic.translation.domain.TranslationRequestList;
 import org.hibernate.sql.Select;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
 
 public interface TranslationAnswerFileRepository extends JpaRepository<TranslationAnswerFile,Long> {
 
@@ -17,4 +20,10 @@ public interface TranslationAnswerFileRepository extends JpaRepository<Translati
 
     @Query("SELECT traf FROM TranslationAnswerFile traf WHERE traf.translationRequestList.trId = :trId")
     TranslationAnswerFile findAnswerFileById(Long trId);
+
+    @Query("SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END FROM TranslationAnswerFile a WHERE a.trAnswerDate IS NULL AND a.translationRequestList = :translationRequestList")
+    boolean existsByTrAnswerDateNullAndTranslationRequestList(@Param("translationRequestList") TranslationRequestList translationRequestList);
+
+    @Query("SELECT a.trAnswerDate FROM TranslationAnswerFile a WHERE a.translationRequestList = :translationRequestList ORDER BY a.trAnswerDate")
+    LocalDate findLatestTrAnswerDateByTranslationRequestList(@Param("translationRequestList") TranslationRequestList translationRequestList);
 }
